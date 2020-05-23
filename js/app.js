ff5cf8db6e043cbd7931ba7301d150022e6a673f
Jordan Reger <jordanreger@users.noreply.github.com>
2020-05-23 11:20:53 -0400

    regup

--- js/app.js
+++ js/app.js
@@ -2,16 +2,10 @@ var userData = {};
 
 window.addEventListener('load', (event) => {
   if (!userSession.isUserSignedIn()) {
-    location.href = '/';
+    /* location.href = '/'; */
   }
 
   userData = userSession.loadUserData();
 
-	for (var i = 0; i < userData.length; i++){
-		if (obj[i] == 'name'){
-			 x = obj[i].name;
-		}
-	}
-
-	document.getElementById('user-name').innerHTML = x;
+	document.getElementById('user-name').innerHTML = userData.username;
 });
